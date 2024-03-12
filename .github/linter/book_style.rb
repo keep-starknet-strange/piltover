af95281ef805a9da01c0d524da66decb2fab5d3e
Brandon Roberts <brandonjroberts22@gmail.com>
2024-03-11 20:50:53 -0500

    Typo exclude

--- .github/linter/book_style.rb
+++ .github/linter/book_style.rb
@@ -4,6 +4,6 @@ exclude_rule 'MD002'
 exclude_rule 'MD041'
 
 # allow inline HTML for book files
-exlude_rule 'MD033'
+exclude_rule 'MD033'
 # Allow long lines in the book for now
 exclude_rule 'MD013'
